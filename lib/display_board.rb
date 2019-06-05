2e91ccc81427f37ad8f6f2e34bb4ec8e2f0ba9f7
Vincent Nguyen <sefton_student@hotmail.com>
2019-06-05 11:14:22 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(state)
+  i = -1
+  puts " #{state[i+=1]} | #{state[i+=1]} | #{state[i+=1]} "
+  puts("-----------")
+  puts " #{state[i+=1]} | #{state[i+=1]} | #{state[i+=1]} "
+  puts("-----------")
+  puts " #{state[i+=1]} | #{state[i+=1]} | #{state[i+=1]} "
+end
