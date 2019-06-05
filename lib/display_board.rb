a4f0938c76310a08b469d3dadb92244d9af621ab
Vincent Nguyen <sefton_student@hotmail.com>
2019-06-05 11:13:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 def display_board(state)
   i = 0
-  puts "#{state[i+=1]} | #{state[i+=1]} | #{state[i+=1]}"
-  print("-----------")
-  puts "#{state[i+=1]} | #{state[i+=1]} | #{state[i+=1]}"
-  print("-----------")
-  puts "#{state[i+=1]} | #{state[i+=1]} | #{state[i+=1]}"
+  puts " #{state[i+=1]} | #{state[i+=1]} | #{state[i+=1]} "
+  print(" ----------- ")
+  puts " #{state[i+=1]} | #{state[i+=1]} | #{state[i+=1]} "
+  print(" ----------- ")
+  puts " #{state[i+=1]} | #{state[i+=1]} | #{state[i+=1]} "
 end
