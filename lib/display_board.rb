f628251ad656404db487ee7efc0f77ac828de864
Vincent Nguyen <sefton_student@hotmail.com>
2019-06-05 11:10:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(state)
+  i = 0
+  puts "#{state[i++]} | #{state[i++]} | #{state[i++]}"
+  print("-----------")
+  puts "#{state[i++]} | #{state[i++]} | #{state[i++]}"
+  print("-----------")
+  puts "#{state[i++]} | #{state[i++]} | #{state[i++]}"
+end
